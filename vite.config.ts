24837e79ecf99c0d7ed05e5b95d5cc2f06f20dde
归 <bugui@guideMacBook-Air.local>
2026-03-08 23:29:08 +0800

    chore: adapt base path for custom domain buguibgib.cn

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   plugins: [react()],
-  base: '/buguibgiblog/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
